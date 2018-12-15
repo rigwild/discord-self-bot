46838d925c93502984db8a4750f2936c6f057b4d
rigwild <contact@asauvage.fr>
2018-12-15 23:42:25 +0100

    Added personal user id to delete module

--- modules/delete.js
+++ modules/delete.js
@@ -4,8 +4,8 @@
  * This module deletes the last messages sent by the user.
  * Usage : delete(userId, amount)
  *
- * @param userId {string} the id of the users to delete messages from
  * @param amount {integer} amount of messages find (others included)
+ * @param userId {string} the id of the users to delete messages from
  * @param before {string} fetch messages before this id
  */
 
@@ -14,15 +14,18 @@ const minArgsCount = 1
 
 // Function started by the module manager
 const start = async (channel, ...commandArgs) => {
-  if (commandArgs[1] && isNaN(commandArgs[1]))
-    throw new Error('The second parameter is not a number')
+  if (commandArgs[0] && isNaN(commandArgs[0]))
+    throw new Error('The first parameter is not a number')
 
-  const amount = parseInt(commandArgs[1] || 50, 10)
+  const amount = parseInt(commandArgs[0] || 50, 10)
+
+  const userId = commandArgs[1] || process.env.personalUserId
+  if (!userId) throw new Error('No user id was specified')
 
   // Fetch messages in the current channel
   const options = { limit: amount, before: commandArgs[2] }
   let messages = await channel.fetchMessages(options)
-  messages = messages.filter(x => x.author.id === commandArgs[0] && x.deletable && !x.deleted)
+  messages = messages.filter(x => x.author.id === userId && x.deletable && !x.deleted)
 
   // Delete the messages with a delay
   let delay = 200
